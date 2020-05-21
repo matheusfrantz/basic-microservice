d9f31f4a5bae9d655cbdc09aedfaf47c7e77dc30
Matheus Frantz <matheus.frantz@ymail.com>
2020-05-21 16:29:10 -0300

    Add ring library

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
   :plugins [[lein-cljfmt "0.6.7"]
             [lein-kibit "0.1.8"]]
 
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-jetty-adapter "1.8.1"]]
 
   :aliases {"lint" ["do" ["kibit"] ["cljfmt" "check"]]}
 
--- src/basic_microservice/server.clj
+++ src/basic_microservice/server.clj
@@ -1,5 +1,12 @@
-(ns basic-microservice.server)
+(ns basic-microservice.server
+  (:use ring.adapter.jetty))
+
+(defn handler
+  [request]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    "Hello, world!"})
 
 (defn -main
   []
-  (println "Hello, world!"))
+  (run-jetty handler {:port 3000}))
