07c993f694fd56d76a188232b2ae06a077b7e003
Matheus Frantz <matheus.frantz@ymail.com>
2020-05-21 15:52:22 -0300

    Add cljfmt plugin

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "A ready to use Clojure application with a minimal configuration."
   :url "https://github.com/matheusfrantz/basic-microservice"
 
-  :plugins [[lein-kibit "0.1.8"]]
+  :plugins [[lein-cljfmt "0.6.7"]
+            [lein-kibit "0.1.8"]]
 
   :dependencies [[org.clojure/clojure "1.10.1"]]
 
-  :aliases {"lint" ["kibit"]}
+  :aliases {"lint" ["do" ["kibit"] ["cljfmt" "check"]]}
 
   :main basic-microservice.server)
