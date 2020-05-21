d079364f0213ad00f1689246325fc1b4c05466b9
Matheus Frantz <matheus.frantz@ymail.com>
2020-05-21 15:44:24 -0300

    Add kibit plugin

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject basic-microservice "1.0.0"
   :description "A ready to use Clojure application with a minimal configuration."
   :url "https://github.com/matheusfrantz/basic-microservice"
+
+  :plugins [[lein-kibit "0.1.8"]]
+
   :dependencies [[org.clojure/clojure "1.10.1"]]
+
+  :aliases {"lint" ["kibit"]}
+
   :main basic-microservice.server)
