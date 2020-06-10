1278aaddbcf5ed481eed06f181722b2c038f5ce5
Matheus Frantz <matheus.frantz@ymail.com>
2020-06-10 09:54:24 -0300

    Add logic test

---
+++ test/basic_microservice/logic/account_test.clj
@@ -0,0 +1,11 @@
+(ns basic-microservice.logic.account-test
+  (:require [basic-microservice.logic.account :as logic.account]
+            [clojure.test :refer :all]))
+
+(deftest new-account-test
+  (testing "creates a new account"
+    (let [name    "John"
+          account (logic.account/new-account name)]
+      (is (some? account))
+      (is (some? (:id account)))
+      (is (= (:name account) name)))))
