51cd4222442cec70555994d88fea2a81be0d16d5
Matheus Frantz <matheus.frantz@ymail.com>
2020-08-13 21:35:40 -0300

    Add integration test (#4)

---
+++ test/integration/account_test.clj
@@ -0,0 +1,36 @@
+(ns integration.account-test
+  (:require [ring.mock.request :as mock]
+            [basic-microservice.service :as service]
+            [clojure.test :refer :all]
+            [integration.helper :as helper]))
+
+(def account (helper/generate-body {:name "John"}))
+
+(deftest ^:integration new-account-test
+  (let [response (service/handler (-> (mock/request :post
+                                                    "/account"
+                                                    account)
+                                      (mock/content-type "application/json")))
+        body     (helper/parse-body (:body response))
+        id       (:id body)]
+
+    (is (= (:status response) 200))
+    (is (= (:name body) "John"))
+
+    (let [response (service/handler (mock/request :get
+                                                  (str "/account/" id)))
+          body     (helper/parse-body (:body response))]
+
+      (is (= (:status response) 200))
+      (is (= (:name body) "John")))
+
+    (let [response (service/handler (mock/request :delete
+                                                  (str "/account/" id)))]
+
+      (is (= (:status response) 204)))))
+
+(deftest ^:integration invalid-account-test
+  (let [response (service/handler (mock/request :get
+                                                "/account/123"))]
+
+    (is (= (:status response) 404))))
