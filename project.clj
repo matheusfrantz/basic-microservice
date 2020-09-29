efa9de18d06684a604f97a84191923da7730d2f5
Matheus Frantz <matheus.frantz@ymail.com>
2020-09-29 10:51:42 -0300

    Bump compojure to 1.6.2

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             [lein-kibit "0.1.8"]]
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [compojure "1.6.1"]
+                 [compojure "1.6.2"]
                  [ring/ring-core "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-jetty-adapter "1.8.1"]
