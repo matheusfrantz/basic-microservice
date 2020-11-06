a8bec157706fbbde9ce5efee836bfb5857de36db
Matheus Frantz <matheus.frantz@ymail.com>
2020-11-06 11:33:38 -0300

    Bump ring-core from 1.8.1 to 1.8.2

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.2"]
-                 [ring/ring-core "1.8.1"]
+                 [ring/ring-core "1.8.2"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-jetty-adapter "1.8.1"]
                  [ring/ring-json "0.5.0"]
