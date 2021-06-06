afbe112ba42dceccb0c24fd6434450cd6e3d2952
Matheus Frantz <matheus.frantz@ymail.com>
2021-06-05 23:52:17 -0300

    Remove integration test

--- test/integration/helper.clj
+++
@@ -1,8 +0,0 @@
-(ns integration.helper
-  (:require [cheshire.core :as json]))
-
-(defn generate-body [body]
-  (json/generate-string body))
-
-(defn parse-body [body]
-  (json/parse-string body true))
