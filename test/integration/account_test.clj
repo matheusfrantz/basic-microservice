ff5ca74c746c6531538ebe1a20f4017da254ed63
Matheus Frantz <matheus.frantz@ymail.com>
2021-06-16 17:33:06 -0300

    Fix integration tests

--- test/integration/account_test.clj
+++ test/integration/account_test.clj
@@ -6,50 +6,46 @@
             [ring.mock.request :as mock]))
 
 (def account (helper/generate-body {:name "John"}))
+(def system (component/start-system!))
+(def handler (service/handler (:storage system)))
 
 (deftest ^:integration new-account-test
-  (let [system  (component/start-system!)
-        handler (service/handler (:storage system))]
+  (let [response (handler (-> (mock/request :post
+                                            "/account"
+                                            account)
+                              (mock/content-type "application/json")))
+        body     (helper/parse-body (:body response))
+        id       (:id body)]
 
-    (let [response (handler (-> (mock/request :post
-                                              "/account"
-                                              account)
-                                (mock/content-type "application/json")))
-          body     (helper/parse-body (:body response))
-          id       (:id body)]
-
-      (is (= 200 (:status response)))
-      (is (= "John" (:name body)))
-
-      (let [response (handler (mock/request :get
-                                            (str "/account/" id)))
-            body     (helper/parse-body (:body response))]
-
-        (is (= 200 (:status response)))
-        (is (= "John" (:name body))))
-
-      (let [response (handler (mock/request :delete
-                                            (str "/account/" id)))]
-
-        (is (= 204 (:status response)))))))
-
-(deftest ^:integration invalid-account-test
-  (let [system  (component/start-system!)
-        handler (service/handler (:storage system))]
-
-    (let [response (handler (-> (mock/request :post
-                                              "/account"
-                                              {})
-                                (mock/content-type "application/json")))]
-
-      (is (= 400 (:status response))))
+    (is (= 200 (:status response)))
+    (is (= "John" (:name body)))
 
     (let [response (handler (mock/request :get
-                                          "/account/123"))]
+                                          (str "/account/" id)))
+          body     (helper/parse-body (:body response))]
 
-      (is (= 404 (:status response))))
+      (is (= 200 (:status response)))
+      (is (= "John" (:name body))))
 
     (let [response (handler (mock/request :delete
-                                          "/account/123"))]
+                                          (str "/account/" id)))]
 
       (is (= 204 (:status response))))))
+
+(deftest ^:integration invalid-account-test
+  (let [response (handler (-> (mock/request :post
+                                            "/account"
+                                            {})
+                              (mock/content-type "application/json")))]
+
+    (is (= 400 (:status response))))
+
+  (let [response (handler (mock/request :get
+                                        "/account/123"))]
+
+    (is (= 404 (:status response))))
+
+  (let [response (handler (mock/request :delete
+                                        "/account/123"))]
+
+    (is (= 204 (:status response)))))
